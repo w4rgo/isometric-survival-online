55c4a15c234cdbbb563f028a120be2074d219c81
Francisco Ruiz <fruiz@gsngames.com>
2017-02-20 13:05:38 +0000

    added dark rift and basic networking

--- isometric-survival-online/Assets/Scripts/Movement/CharacterMovement.cs
+++ isometric-survival-online/Assets/Scripts/Movement/CharacterMovement.cs
@@ -6,6 +6,7 @@ namespace Movement
     public class CharacterMovement : MonoBehaviour
 
     {
+        public bool isControllable;
 
         Animator anim;
         public float speed = 5f;
@@ -13,82 +14,33 @@ namespace Movement
         private Vector3 previousPosition = Vector3.zero;
 
         // Use this for initialization
-        void Start () {
+        void Start()
+        {
             anim = GetComponent<Animator>();
             //GetComponent<Rigidbody2D>().velocity = movement;
         }
 
         // Update is called once per frame
-        void FixedUpdate () {
-            float inputX = Input.GetAxisRaw("Horizontal");
-            float inputY = Input.GetAxisRaw("Vertical");
-            movement = new Vector2(inputX, inputY);
-            transform.Translate(movement * speed * Time.deltaTime);
-            var currentPos = transform.position;
+        void FixedUpdate()
+        {
+            if (isControllable)
+            {
+                float inputX = Input.GetAxisRaw("Horizontal");
+                float inputY = Input.GetAxisRaw("Vertical");
+                movement = new Vector2(inputX, inputY);
+                transform.Translate(movement * speed * Time.deltaTime);
+                var currentPos = transform.position;
 
-            //Diagonals
-            anim.SetFloat("speedX",movement.x);
-            anim.SetFloat("speedY",movement.y);
+                anim.SetFloat("speedX", movement.x);
+                anim.SetFloat("speedY", movement.y);
+//                var angleBetween = (Mathf.Atan2(previousPosition.y - currentPos.y, previousPosition.x - currentPos.x) *
+//                                    180 /
+//                                    Math.PI) + 90;
+                previousPosition = currentPos;
 
-//            if (inputX != 0 && inputY != 0) {
-//                if (movement.y == 1 && movement.x == -1)
-//                {
-//                    anim.SetTrigger("move_up_left");
-//                }
-//
-//                if (movement.y == 1 && movement.x == 1)
-//                {
-//                    anim.SetTrigger("move_up_right");
-//                }
-//
-//                if (movement.y == -1 && movement.x == -1)
-//                {
-//                    anim.SetTrigger("move_down_left");
-//                }
-//
-//                if (movement.y == -1 && movement.x == 1)
-//                {
-//                    anim.SetTrigger("move_down_right");
-//                }
-//            }
-//
-//            else
-//            {
-//
-//                //left/right/up/down
-//                if (movement.x == -1)
-//                {
-//                    anim.SetTrigger("move_left");
-//                }
-//
-//                if (movement.x == 1)
-//                {
-//                    anim.SetTrigger("move_right");
-//                }
-//
-//
-//                if (movement.y == 1)
-//                {
-//                    anim.SetTrigger("move_up");
-//                }
-//
-//
-//                if (movement.y == -1)
-//                {
-//                    anim.SetTrigger("move_down");
-//                }
-//            }
+//                anim.SetFloat("direction", (float) angleBetween);
+            }
 
-
-
-
-
-            var angleBetween = (Mathf.Atan2(previousPosition.y - currentPos.y, previousPosition.x - currentPos.x) * 180 /
-                               Math.PI)+90;
-            previousPosition = currentPos;
-
-            anim.SetFloat("direction", (float) angleBetween);
         }
-
     }
 }
